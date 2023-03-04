f2a34cfd84a117d1e770b3189a094ea9a504525c
Moranti1 <125757084+Moranti1@users.noreply.github.com>
2023-03-04 20:30:06 +0800

    Bank
    Это Java Web-приложение для банковской системы, которое позволяет пользователям управлять своими банковскими счетами, переводить деньги и просматривать историю своих транзакций. Это приложение разработано с использованием технологий Java, Spring Framework
    Функциональность:
    Регистрация и авторизация пользователей
    Просмотр баланса на счете
    Пополнение и снятие денег со счета
    Перевод денег между счетами пользователей
    Просмотр истории транзакций
    Приложение имеет два типа пользователей: администратор и обычный пользователь. Администратор может управлять всеми пользователями и счетами, а также имеет доступ к системной информации. Обычный пользователь может только управлять своими счетами и выполнять транзакции.
    Технологии:
    Java
    Spring Framework
    Это приложение может быть использовано банками для управления банковскими операциями и клиентскими данными. Оно обеспечивает быстрый, надежный и безопасный доступ к банковским услугам для пользователей в Интернете.

---
+++ MyProjectBank/src/main/java/com/example/myprojectbank/BalanceController.java
@@ -0,0 +1,36 @@
+package com.example.myprojectbank;
+
+import com.example.myprojectbank.model.TransferBalance;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+@Slf4j
+@RestController("/balance")
+@AllArgsConstructor
+public class BalanceController {
+
+    private final BankService bankService;
+
+
+    @GetMapping("/{accountId}")
+    public BigDecimal getBalance(@PathVariable Long accountId){
+        return bankService.getBalance(accountId);
+    }
+    @PostMapping("/add")
+    public BigDecimal addMoney(@RequestBody TransferBalance transferBalance){
+        return bankService.addMoney(transferBalance.getTo(), transferBalance.getAmount());
+    }
+
+    @PostMapping("/transfer")
+    public void transfer(@RequestBody TransferBalance transferBalance){
+        bankService.makeTransfer(transferBalance);
+    }
+    @ExceptionHandler(IllegalAccessException.class)
+    public String handle(IllegalAccessException e){
+        log.error(e.getMessage());
+        return "no money";
+
+    }
+}
---
+++ MyProjectBank/src/test/java/com/example/myprojectbank/MyProjectBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myprojectbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyProjectBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
